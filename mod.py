00e4d1199c829cba145cca679aa46fd3093f0291
Joemnn <josue.travez22@em-normandie.fr>
2024-11-16 03:15:15 +0100

    mod.py

---
+++ mod.py
@@ -0,0 +1,11 @@
+# 7.4.1 Create a python module mod.
+# In mod.py, create a function psum(number1, number2) that takes two arguments and prints the sum of them. Assume the input arguments to be numbers.
+# In mod.py, create a function pmultiply(number1, number2) that takes two arguments and prints the product of them. Assume the input arguments to be numbers.
+#mod
+def psum(number1, number2):
+  """Prints the sum of two numbers"""
+  print(f"The sum of {number1} and {number2} is: {number1 + number2}")
+
+def pmultiply(number1, number2):
+  """Prints the product of two numbers"""
+  print(f"The product of {number1} and {number2} is: {number1 * number2}")
